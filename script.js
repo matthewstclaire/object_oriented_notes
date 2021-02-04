4c4c737ca83bbd4eaaaf0d65a28aeebc796849c5
matthewstclaire <matthew.crnich@gmail.com>
2021-02-04 15:37:27 -0700

    Sample code for constructor and protype

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const matt = new Person('Matt', 1993);
+
+//prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+matt.calcAge();
