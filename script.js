b53703e63bec76386f2b68932269ba6ac2e89f5d
matthewstclaire <matthew.crnich@gmail.com>
2021-02-05 12:53:53 -0700

    Coding challenge complete

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const matt = new Person('Matt', 1993);
+
+// //prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// matt.calcAge();
+
+// 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+// 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+// DATA CAR 1: 'BMW' going at 120 km/h
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const matt = new Person('Matt', 1993);
-
-//prototypes
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.speed} is how much faster you are going.`);
 };
 
-matt.calcAge();
+const car1 = new Car('bmw', 120);
+
+car1.accelerate();
+car1.accelerate();
