0ae2416fc0cc2ccbe790ee025d9036ad06cd11df
matthewstclaire <matthew.crnich@gmail.com>
2021-02-07 23:04:01 -0700

    basic constructor laid out

--- script.js
+++ script.js
@@ -54,20 +54,57 @@
 // matt.calcAge();
 
 //reading properties. Awesome way to do it.
-const account = {
-  owner: 'Matt',
-  movements: [200, 530, 120, 300],
+// const account = {
+//   owner: 'Matt',
+//   movements: [200, 530, 120, 300],
 
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
-  //always needs one param
-  set latest(mov) {
-    this.movements.push(mov);
-  },
-};
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+//   //always needs one param
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
 
-console.log(account.latest);
+// console.log(account.latest);
 
-account.latest = 50;
-console.log(account.movements);
+// account.latest = 50;
+// console.log(account.movements);
+
+// //creating static method
+// static hey() {
+//     console.log('Hey there')
+//     console.log(this)
+// }
+
+// 1. Re-create challenge 1, but this time using an ES6 class;
+// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+// 4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+// DATA CAR 1: 'Ford' going at 120 km/h
+
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+// }
+// const ford = new Car('Ford', 120);
+
+// console.log(ford.speedUS);
+
+//use extend for inheriting with es6 class
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+  }
+}
